5d0be3956c302245c55d4443dbac581b8f7fb0bc
shawnohn <developer.ohn@gmail.com>
2020-09-29 23:08:21 -0400

    Add projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,4 +11,28 @@ export default [
       },
     ],
   },
+  {
+    slug: 'https://loving-sitter.herokuapp.com/',
+    title: 'Loving-sitter',
+    description: 'Connecting pets lovers and sitters',
+    year: '2020',
+    links: [
+      {
+        label: 'GitHub',
+        url: 'https://github.com/shawnohn/pet-service-app',
+      },
+    ],
+  },
+  {
+    slug: 'https://project-tango-shawn.herokuapp.com/',
+    title: 'Google Forms Clone',
+    description: 'A basic version of Google Forms',
+    year: '2020',
+    links: [
+      {
+        label: 'GitHub',
+        url: 'https://github.com/shawnohn/Project-Tango',
+      },
+    ],
+  },
 ]
