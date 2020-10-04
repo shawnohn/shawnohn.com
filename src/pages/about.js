2cad53fd8edab90ab0fdef0e7418b3f0b90f8956
shawnohn <developer.ohn@gmail.com>
2020-10-04 00:44:19 -0400

    Edit about-me data

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,10 +17,12 @@ const About = (_) => {
             and Microsoft SQL Server in South Korea. After graduation with a
             Master of Applied Computing at the University of Windsor, I worked
             as an independent Unity Game Developer(1 year) and it has turned my
-            creative work as a game enthusiast. Now I'm breaking into the job
-            market in Canada as a Full-Stack Web Developer. I prefer JavaScript
-            frameworks to jump into the projects on going without hesitation and
-            to perform the best.
+            creative work as a game enthusiast.
+          </p>
+          <p>
+            Now I'm breaking into the job market in Canada as a Full-Stack Web
+            Developer. I prefer JavaScript frameworks to jump into the projects
+            on going without hesitation and to perform the best.
           </p>
         </article>
       </div>
